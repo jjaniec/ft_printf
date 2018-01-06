bb2830faf17c1caef341cc3617e61301de0ca2e7
Joffrey Janiec <jjaniec@student.42.fr>
2018-01-06 18:22:50 +0100

    add putwchar func working for 1 and 2 bytes wchar_ts

--- srcs/ft_putwchar.c
+++ srcs/ft_putwchar.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/05 20:19:38 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/01/05 21:21:11 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/01/06 18:20:59 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,9 @@ void	ft_putwchar(wchar_t c)
 		ft_putchar((char)c);
 	else if (c <= 0x7FF)
 	{
-		
-	}
+		ft_putchar((unsigned char)((c >> 6) | 0xC0));
+		ft_putchar((unsigned char)((c & 0x3F) | 0x80));
+	}/*
 	else if (c <= 0xFFFF)
 	{
 		
@@ -27,5 +28,25 @@ void	ft_putwchar(wchar_t c)
 	else if (c <= 0x1FFFFF)
 	{
 		
-	}
+	}*/
+}
+
+int		main()
+{
+	ft_putwchar('a');
+	ft_putwchar(0xA9);
+	ft_putwchar(0x851);
+	ft_putwchar(0xFFFFF);
+
+
+	unsigned char c;
+    c = 0xe1;
+    write(1, &c, 1);
+    c = 0x88;
+    write(1, &c, 1);
+    c = 0xb4;
+    write(1, &c, 1);
+    c = 0x0a;
+    write(1, &c, 1);
+	return (0);
 }
