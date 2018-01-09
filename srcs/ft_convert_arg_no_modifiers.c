4ca1406d541163401356f0172cbf5cf48492f83f
Joffrey Janiec <jjaniec@student.42.fr>
2018-01-09 15:05:29 +0100

    add conversion wchar_t ptr -> char ptr - update test main

--- srcs/ft_convert_arg_no_modifiers.c
+++ srcs/ft_convert_arg_no_modifiers.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/12 14:25:16 by jjaniec           #+#    #+#             */
-/*   Updated: 2017/12/16 18:51:01 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/01/09 14:34:53 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 
 void	ft_fill_function_pointers_tab(char *(**f)(void *))
 {
-	//f[('S' - 'A')] = ft_wchat_t_to_str//alias de ls
+	f[('S' - 'A')] = &ft_wchar_t_to_str;//alias de ls
 	//f[('p' - 'A')] = &ft_void_to_hex;
 	f[('d' - 'A')] = &ft_itoa;
 	//f[('D' - 'A')] = &ft_itoa(ft_longint_to_signedint()); //alias ld
@@ -54,6 +54,8 @@ char	*ft_convert_arg_no_modifiers(va_list va_ptr, char *fl)
 			return ((*f[*fl - 'A'])((int)va_arg(va_ptr, int)));
 		else if (*fl == 'o' || *fl == 'u' || *fl == 'x' || *fl == 'X')
 			return ((*f[*fl - 'A'])((unsigned int)va_arg(va_ptr, unsigned int)));
+		else if (*fl == 'S' || (fl[0] == 'l' && fl[1] == 's'))
+			return ((*f[*fl - 'A'])((wchar_t *)va_arg(va_ptr, wchar_t *)));
 		else if (*fl == 'p')
 			return ((*f[*fl - 'A'])((void *)va_arg(va_ptr, void *)));
 	return (NULL);
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/11 20:50:34 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/01/05 19:24:46 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/01/09 14:54:28 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,16 @@ int		main(int ac, char **av)
 {
 	char	*s;
 	int		i;
+	wchar_t q[2];
+
+	q[0] = 0x100D7;
+	q[1] = '\0';
 
 	i = 0;
 	s = "char |%c|\ndec |%d|\nrand+str rokdrgopih|%s|\n hex & hex caps |%x| |%X|\nrand+fake e%wfdwia%qjirg";
 	ft_printf(s, 'X', 12, "debug", 255, 255);
 
-	ft_printf("\nRslt ui%dnttoh%%ex: %s\n", 12, ft_uint_to_hex(1));
+	ft_printf("\nRslt ui%dnttoh%%ex%S: %s\n", 12, &q[0], ft_uint_to_hex(1));
 
 	//printf("%s", ft_uint_to_str(4294967296));
 	return (0);
