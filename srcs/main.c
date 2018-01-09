263178bbc31d5e0cc64327ef18360f7124fccb78
Joffrey Janiec <jjaniec@student.42.fr>
2018-01-09 16:17:14 +0100

    add conversion wint_t -> char * in printf - update test main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/11 20:50:34 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/01/09 14:54:28 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/01/09 16:16:08 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int		main(int ac, char **av)
 	s = "char |%c|\ndec |%d|\nrand+str rokdrgopih|%s|\n hex & hex caps |%x| |%X|\nrand+fake e%wfdwia%qjirg";
 	ft_printf(s, 'X', 12, "debug", 255, 255);
 
-	ft_printf("\nRslt ui%dnttoh%%ex%S: %s\n", 12, &q[0], ft_uint_to_hex(1));
+	ft_printf("\nRslt ui%dnttoh%%ex%S%C: %s\n", 12, &q[0], q[0], ft_uint_to_hex(1));
 
 	//printf("%s", ft_uint_to_str(4294967296));
 	return (0);
