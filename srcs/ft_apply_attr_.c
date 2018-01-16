039cdabd3b948d6ec54a8d1f0236b0bb5b0cee78
Joffrey Janiec <jjaniec@student.42.fr>
2018-01-16 18:43:51 +0100

    add ft_apply_attr_space function - update header

--- srcs/ft_apply_attr_.c
+++ srcs/ft_apply_attr_.c
@@ -6,11 +6,34 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/16 16:25:24 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/01/16 17:49:25 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/01/16 18:43:25 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
+
+/*
+** Apply attribute space on content of arg $e
+*/
+
+void	 ft_apply_attr_space(t_arg **e)
+{
+    char    *s;
+    char    *a;
+
+    if (((*e)->flag[0] == 'd' || \
+          (*e)->flag[0] == 'i'))
+    {
+        a = malloc(sizeof(char) * 2);
+        a[1] = '\0';
+        if (ft_atoi((*e)->data_converted) >= 0)
+            a[0] = ' ';
+        else
+            a[0] = '-';
+        s = ft_strjoin_free(a, (*e)->data_converted);
+        (*e)->data_converted = s;
+    }   
+}
 
 /*
 ** Apply minus attribute on content of arg $e
