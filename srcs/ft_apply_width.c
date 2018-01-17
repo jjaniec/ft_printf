8346a1bdf67f4f367814553d128aef5a26ff57d3
Joffrey Janiec <jjaniec@student.42.fr>
2018-01-17 20:05:59 +0100

    add ft_swap_bytes to place sign characters at the beginning of the string

--- srcs/ft_apply_width.c
+++ srcs/ft_apply_width.c
@@ -6,25 +6,43 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/17 17:45:52 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/01/17 18:31:45 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/01/17 20:04:55 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
 /*
+** Swap bytes of the postive or negative char before the number and the behind
+** right before it
+*/
+
+static void     ft_byte_swap(char *a, char *b)
+{
+    char    t;
+
+    t = *b;
+    *b = *a;
+    *a = t;
+}
+
+/*
 ** Resize the char * of the data converted of $e as specified in width option
 */
 
-void    ft_apply_width(t_arg **e)
+void            ft_apply_width(t_arg **e)
 {
     char    c;
+    char    cx;
     char    *s;
     int     l;
 
+    cx = 0;
     l = ft_atoi((*e)->width) - ft_strlen((*e)->data_converted) + 1;
     if (l >= 2)
     {
+        if ((*e)->data_converted[0] == '-' || (*e)->data_converted[0] == '+')
+            cx = (*e)->data_converted[0];
         if ((*e)->attributes && \
             ft_strchr((*e)->attributes, '0') && !ft_strchr((*e)->attributes, '-'))
             c = '0';
@@ -36,7 +54,7 @@ void    ft_apply_width(t_arg **e)
             s = ft_strjoin_free(ft_generate_width(l, c), (*e)->data_converted);
         if (s)
             (*e)->data_converted = s;
-
+        if (cx)
+            ft_byte_swap(ft_strchr((*e)->data_converted, cx), &(*e)->data_converted[0]);
     }
-
 }
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/11 20:50:34 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/01/16 21:13:09 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/01/17 20:01:24 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int		main(void)
   
 	i = 0;
     ft_printf("%%");
-	s = "char - char uni|%0.c|%000+++8.454C||\ndec |% +05d||%+ 010i|\nrand+str -struni rokdrgopih|%+s||%4.8445S|\n hex & hex caps |%+x| |% X|\nrand+fake e%wfdwia%qjirg\noctal o|%0 o|\nuint |%0##u|";
+	s = "char - char uni|%0.c|%000+++-8.454C||\ndec |%++010d||%+++0 2i|\nrand+str -struni rokdrgopih|%+s||%4.8445S|\n hex & hex caps |%+x| |% X|\nrand+fake e%wfdwia%qjirg\noctal o|%0 o|\nuint |%0u|";
     setlocale(LC_ALL, "");
 	ft_printf(s,\
         'X',
@@ -41,7 +41,7 @@ int		main(void)
         255,
         4242
         );
-	//printf(s, 'X', q[0], 12, 12, "debug", q, 255, 255, 255, 4242);
+	printf(s, 'X', q[0], 12, -12, "debug", q, 255, 255, 255, 4242);
 	//printf("\nnoargs + blank\n");
 
 	return (0);
