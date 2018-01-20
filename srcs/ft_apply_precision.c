9708b4737c57de110caa4afb16a76ca086c169c4
Joffrey Janiec <jjaniec@student.42.fr>
2018-01-20 15:30:37 +0100

    apply_precision for 's' conv

--- srcs/ft_apply_precision.c
+++ srcs/ft_apply_precision.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/20 14:05:23 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/01/20 14:40:39 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/01/20 15:24:51 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,8 @@
 */
 
 void    ft_apply_precision(t_arg **e)
-{/*
-    if (ft_strlen((*e)->data_converted) > ft_atoi((*e)->precision))
-        ft_apply_precision_sub(e);
-    else if (ft_strlen((*e)->data_converted) < ft_atoi((*e)->precision))
-        ft_apply_precision_add(e);
-*/
+{
+    if (ft_strchr((*e)->flag, 's'))
+        (*e)->data_converted = ft_strsub_free(\
+            (*e)->data_converted, 0, ft_atoi((*e)->precision));
 }
