b0588fe2001c2476bb23f1fdf95ad5e783c685d7
Joffrey Janiec <jjaniec@student.42.fr>
2018-01-21 17:18:27 +0100

    add lc support

--- srcs/ft_convert_arg_modifiers.c
+++ srcs/ft_convert_arg_modifiers.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/21 15:08:13 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/01/21 16:56:51 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/01/21 17:17:46 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ char    *ft_convert_arg_modifiers(va_list va_ptr, t_arg **e)
         if (ft_strcmp("l", (*e)->modifiers) == 0)
             if ((*e)->flag[0] == 'u')
                 return (ft_imax_toa(va_arg(va_ptr, unsigned long int)));
+            if ((*e)->flag[0] == 'c')
+                return (ft_wchar_t_to_str(va_arg(va_ptr, wint_t)));
             if (ft_is_conv_numeric(e))
                 return (ft_imax_toa(va_arg(va_ptr, long int)));
     }
