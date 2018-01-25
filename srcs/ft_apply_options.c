190f54c232e875df590290323af328a0fe9e73f3
Joffrey Janiec <jjaniec@student.42.fr>
2018-01-25 15:06:19 +0100

    apply # with priority and on big o conversion

--- srcs/ft_apply_options.c
+++ srcs/ft_apply_options.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/16 14:43:43 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/01/24 16:30:19 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/01/25 15:02:08 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@
 
 void	ft_apply_options(t_arg **e)
 {
+	if ((*e)->attributes && ft_strchr((*e)->attributes, '#'))
+        ft_apply_attr_hashtag(e);
 	if ((*e)->precision && (*e)->flag[0] != 'c')
 		ft_apply_precision(e);
 	if ((*e)->attributes)
