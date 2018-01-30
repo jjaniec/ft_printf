9344ef936f1d27a5c9155b600d0772c361410cb1
Joffrey Janiec <jjaniec@student.42.fr>
2018-01-30 13:33:55 +0100

    norme ft_apply_precision - fix conv p w/ 0 precision

--- srcs/ft_apply_precision.c
+++ srcs/ft_apply_precision.c
@@ -6,11 +6,15 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/20 14:05:23 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/01/29 21:51:09 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/01/30 13:29:31 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
+
+/*
+** Handle exceptions cases for o/O w/ # attribute
+*/
 
 static int		ft_can_ret_0(t_arg **e)
 {
@@ -23,37 +27,60 @@ static int		ft_can_ret_0(t_arg **e)
 }
 
 /*
+** Apply precision on non numeric conversions (s/p)
+*/
+
+static void     ft_apply_precision_nonnumeric(t_arg **e)
+{
+    
+	if (*((*e)->flag) == 's')
+		(*e)->data_converted = ft_strsub_free(\
+			(*e)->data_converted, 0, ft_atoi((*e)->precision));
+    if (*((*e)->flag) == 'p' && (*e)->precision && *((*e)->precision) == '.')
+    {
+        free((*e)->data_converted);
+        (*e)->data_converted = ft_strdup("0x");
+    }
+}
+
+/*
+** Apply precision on numeric conversions (d/u/i/x/o/U/X/D/O)
+*/
+
+static void     ft_apply_precision_numeric(t_arg **e)
+{
+    int     x;
+    int     y;
+
+    x = 0;
+    y = 0;
+	if (ft_strchr((*e)->precision, '.') && \
+		ft_strcmp((*e)->data_converted, "0") == 0 && ft_can_ret_0(e))
+	{
+		free((*e)->data_converted);
+		(*e)->data_converted = ft_strdup("");
+	}
+	else
+	{
+		y = ft_atoi((*e)->data_converted);
+		x = (ft_atoi((*e)->precision) - ft_strlen((*e)->data_converted) + \
+			((y >= 0) ? (1) : (2)));
+		if (x > 0)
+			(*e)->data_converted = \
+			ft_strjoin_free(ft_generate_width(x, '0'), (*e)->data_converted);
+		if (y < 0)
+			ft_byte_swap(ft_strchr((*e)->data_converted, '-'), &(*e)->data_converted[0]);
+       }
+}
+
+/*
 ** Apply precision on converted element
 */
 
 void			ft_apply_precision(t_arg **e)
 {
-	int		x;
-	int		y;
-
-	y = 0;
-	x = 0;
-	if (ft_strchr((*e)->flag, 's'))
-		(*e)->data_converted = ft_strsub_free(\
-			(*e)->data_converted, 0, ft_atoi((*e)->precision));
-	if (ft_is_conv_numeric(e))
-	{
-		if (ft_strchr((*e)->precision, '.') && \
-			ft_strcmp((*e)->data_converted, "0") == 0 && ft_can_ret_0(e))
-		{
-			free((*e)->data_converted);
-			(*e)->data_converted = ft_strdup("");
-		}
-		else
-		{
-			y = ft_atoi((*e)->data_converted);
-			x = (ft_atoi((*e)->precision) - ft_strlen((*e)->data_converted) + \
-				((y >= 0) ? (1) : (2)));
-			if (x > 0)
-				(*e)->data_converted = \
-				ft_strjoin_free(ft_generate_width(x, '0'), (*e)->data_converted);
-			if (y < 0)
-				ft_byte_swap(ft_strchr((*e)->data_converted, '-'), &(*e)->data_converted[0]);
-		}
-	}
+    if (!ft_is_conv_numeric(e))
+        ft_apply_precision_nonnumeric(e);
+    else
+        ft_apply_precision_numeric(e);
 }
