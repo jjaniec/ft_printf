5138f669635b1003d06faacce1fddf8c3606c51c
Joffrey Janiec <jjaniec@student.42.fr>
2018-02-09 20:09:22 +0100

    fix negative wide chars as C or lc

--- srcs/ft_wchar_masklen.c
+++ srcs/ft_wchar_masklen.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 14:14:21 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/02/09 18:56:19 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/02/09 20:06:44 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@
 
 int		ft_wchar_masklen(wint_t c)
 {
+    if (c < 255 && !ft_isprint(c))
+        return (1);
 	if (c < 0x0 || (c >= 0xD800 && c < 0xE000) || \
-        (c >= 0x7F && MB_CUR_MAX == 1))
+        (c >= 0x7F && MB_CUR_MAX == 1) || c >= 0x110000)
 		return (-1);
 	if (c <= 0x7F)
 		return (1);
--- srcs/ft_wchar_t_to_str.c
+++ srcs/ft_wchar_t_to_str.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/09 15:13:19 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/02/09 19:45:08 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/02/09 20:06:29 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,10 @@ char	*ft_wchar_t_to_str(wint_t c)
 	if (!c)
 		return (ft_strdup("\0"));
 	masklen = ft_wchar_masklen(c);
-	if (masklen == 1 || (c < 255 && (!ft_isprint(c))))
-		return (ft_char_to_str(c));
-	if (masklen == -1)
+	if (masklen == -1 || c < 0)
 		return (NULL);
+	if (masklen == 1)
+		return (ft_char_to_str(c));
 	i = -1;
 	s = (char *)malloc(masklen + 1);
 	while (++i < masklen)
