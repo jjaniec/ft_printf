f0ec62c7222ae5083f0060eddf5e37bb18529c3d
Joffrey Janiec <jjaniec@student.42.fr>
2018-02-09 21:26:51 +0100

    detect error and free arglist at next arg

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/11 13:44:01 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/02/09 17:13:49 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/02/09 21:25:37 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,17 +28,17 @@ int		ft_printf(const char *restrict format, ...)
 	va_start(va_ptr, format);
     //printf("MB_CUR_MAX : %u\n", MB_CUR_MAX);
 	args = ft_create_arglist(va_ptr, format);
-   // ft_debug_args(format, args);
+    //ft_debug_args(format, args);
 	r = 0;
 	i = 0;
 	l = (int)ft_strlen(format);
 	while (format[i] && i < l)
 	{
 		while (format[i] == '%' && ft_is_flag(format, i))
-            if (!args)
-                return (-1);
-            else
+            if (args && args->flag && *(args->flag) != '!')
                 ft_print_next_arg(&args, format, &i, &r);
+            else
+                return ((!ft_free_arglist(args, va_ptr)) ? (-1) : (-1));
 		if (format[i])
 		{
 			if (format[i] == '%')
