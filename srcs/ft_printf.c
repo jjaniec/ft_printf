87d1527858134463d2113bfb046669f90ca64b81
Joffrey Janiec <jjaniec@student.42.fr>
2018-02-13 18:30:07 +0100

    norme ft_printf.c - addft_printf_not_conv

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,18 +6,31 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/11 13:44:01 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/02/13 16:46:50 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/02/13 18:29:08 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
 /*
-** My recoding of the printf function, return -1 if an error occurs,
+** If format[*i] is equal to '%' and not a conversion, create a temp element
+** to apply options on it with ft_print_percent, otherwise print format[*i]
+*/
+
+static void		ft_printf_not_conv(const char *restrict format, int *i, int *r)
+{
+	if (format[*i] == '%')
+		ft_printf_percent(format, i, r);
+	ft_putchar_printf(format[*i], r);
+}
+
+/*
+** Main function of my recoding of the printf function,
+** return -1 if an error occurs,
 ** otherwise return the lenght of printed string
 */
 
-int		ft_printf(const char *restrict format, ...)
+int				ft_printf(const char *restrict format, ...)
 {
 	int		i;
 	int		l;
@@ -38,11 +51,7 @@ int		ft_printf(const char *restrict format, ...)
 			else
 				return ((ft_print_until_validconv(format, i, args)));
 		if (format[i] && !(args && args->flag && *(args->flag) == '!'))
-		{
-			if (format[i] == '%')
-				ft_printf_percent(format, &i, &r);
-			ft_putchar_printf(format[i], &r);
-		}
+			ft_printf_not_conv(format, &i, &r);
 	}
 	ft_free_arglist(args, va_ptr);
 	return (r);
