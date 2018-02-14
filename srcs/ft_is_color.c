1b4bbe8ea2d09fd04e5b89f21917138114027a6d
Joffrey Janiec <jjaniec@student.42.fr>
2018-02-14 17:15:16 +0100

    fixes in ft_print_color_id with invalid specifiers

--- srcs/ft_is_color.c
+++ srcs/ft_is_color.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/13 22:49:56 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/02/14 15:09:20 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/02/14 16:47:39 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,44 +17,43 @@
 ** if not found, return -1 and print string
 */
 
-static int  ft_parse_prefixes(char *cl)
+static int	ft_parse_prefixes(char *cl)
 {
-    if (!((cl[0] == 'F' || cl[0] == 'B') && \
-        cl[1] == 'G' && cl[2] == '_'))
-        return (-1);
-    else
-        return ((cl[0] == 'F') ? (1) : (2));
+	if (!((cl[0] == 'F' || cl[0] == 'B') && \
+		cl[1] == 'G' && cl[2] == '_'))
+		return (-1);
+	else
+		return ((cl[0] == 'F') ? (1) : (2));
 }
 
 /*
 ** Parse prefix of color "LIGHT" or "DARK"
 */
 
-static int  ft_parse_cl_attribute(char *cl)
+static int	ft_parse_cl_attribute(char *cl)
 {
-    if (strncmp(cl, "LIGHT_", 6) == 0)
-        return (2);
-    if (strncmp(cl, "DARK_", 5) == 0)
-        return (1);
-    return (0);
+	if (strncmp(cl, "LIGHT_", 6) == 0)
+		return (2);
+	if (strncmp(cl, "DARK_", 5) == 0)
+		return (1);
+	return (0);
 }
 
 /*
 ** Return 1 if $cl is a defined color, otherwise return 0
 */
 
-int          *ft_is_color(char *cl)
+int			*ft_is_color(char *cl)
 {
-    int     i;
-    int     attr_id;
-    int     prefx_id;
-    int     *ret;
+	int		attr_id;
+	int		prefx_id;
+	int		*ret;
 
-    if ((prefx_id = ft_parse_prefixes(cl)) == -1)
-        return (NULL);
-    attr_id = ft_parse_cl_attribute(cl + ((prefx_id != 0) ? (3) : (0)));
-    ret = malloc(sizeof(int) * 2);
-    ret[0] = prefx_id;
-    ret[1] = attr_id;
-    return (&(ret[0]));
+	if ((prefx_id = ft_parse_prefixes(cl)) == -1)
+		return (NULL);
+	attr_id = ft_parse_cl_attribute(cl + ((prefx_id != 0) ? (3) : (0)));
+	ret = malloc(sizeof(int) * 2);
+	ret[0] = prefx_id;
+	ret[1] = attr_id;
+	return (&(ret[0]));
 }
--- srcs/ft_print_color.c
+++ srcs/ft_print_color.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/11 20:42:58 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/02/14 15:05:37 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/02/14 17:12:05 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@
 ** return NULL
 */
 
-static char    *ft_parse_specifier(const char *restrict format, int *pos)
+static char		*ft_parse_specifier(const char *restrict format, int *pos)
 {
-    int     i;
+	int		i;
 
-    i = (*pos) + 1;
-    while (format[i] && format[i] != '}')
-        i++;
-    if (!format[i])
-        return (NULL);
-    return (ft_strsub(format + (*pos) + 1, 0, i - 1 - *pos));
+	i = (*pos) + 1;
+	while (format[i] && format[i] != '}')
+		i++;
+	if (!format[i])
+		return (NULL);
+	return (ft_strsub(format + (*pos) + 1, 0, i - 1 - *pos));
 }
 
 /*
@@ -36,20 +36,20 @@ static char    *ft_parse_specifier(const char *restrict format, int *pos)
 ** the function print the color and increment *pos to skip it in format
 */
 
-int 	ft_print_color(const char *restrict format, int *pos)
+int				ft_print_color(const char *restrict format, int *pos)
 {
-    char    *cl;
-    int     *tab;
+	char	*cl;
+	int		*tab;
 
-    tab = NULL;
-    cl = ft_parse_specifier(format, pos);
-    if (!cl)
-        return (0);
-    tab = ft_is_color(cl);
-    if (!tab)
-        return (0);
-    printf("\nparsed color : |%s|\n", cl + 3);
-    ft_print_color_id(cl, tab);
-    *pos += ft_strlen(cl) + 2;
-    return (1);
+	tab = NULL;
+	cl = ft_parse_specifier(format, pos);
+	if (!cl)
+		return (0);
+	tab = ft_is_color(cl);
+	if (!tab)
+		return (0);
+	printf("\nparsed color : |%s|\n", cl + 3);
+	ft_print_color_id(cl + 3, tab);
+	*pos += ft_strlen(cl) + 2;
+	return (1);
 }
